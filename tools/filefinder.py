3a7ab78187d09bc1d275d43738d63954cdc436df
Misha Turnbull <mishaturnbull@gmail.com>
2018-11-18 22:45:31 -0600

    Add generalized version of aspfinder for any file type
    Signed-off-by: Misha Turnbull <mishaturnbull@gmail.com>

---
+++ tools/filefinder.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+import sys
+import os
+
+FILENAME = sys.argv[1]
+FILETYPE = sys.argv[2]
+
+with open(FILENAME, 'r') as f:
+    lines = f.readlines()
+
+lines = [line.strip() for line in lines]
+asplines = [line for line in lines if FILETYPE in line]
+aspparts = [line.split(' ') for line in asplines]
+
+asps = []
+
+for asppart in aspparts:
+    has_asp = list(map(lambda q: FILETYPE in q, asppart))
+    idx = has_asp.index(True)
+    part = asppart[idx]
+    subparts = part.split("'")
+    has_asp = list(map(lambda q: FILETYPE in q, subparts))
+    idx = has_asp.index(True)
+    asp = subparts[idx]
+    asps.append(asp)
+ 
+for asp in asps:
+    print(asp)
+
